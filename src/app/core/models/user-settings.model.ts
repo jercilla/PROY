7f4c5e92776b2cf0132c95762113ff7f17143f6b
alexmaestre <you@example.com>
2025-11-21 00:12:52 +0100

    Correct attribute name bug in UserSettings interface

--- src/app/core/models/user-settings.model.ts
+++ src/app/core/models/user-settings.model.ts
@@ -2,5 +2,5 @@ export interface UserSettings {
   length: number;       // longitud del texto (entre 120 y 1024)
   tone: 'Muy Informal' | 'Informal' | 'Formal' | 'Muy Formal'; // tono del texto
   emojis: boolean;      // si se usan emojis
-  hashtag: boolean;     // si se usan hashtags
+  hashtags: boolean;     // si se usan hashtags
 }
