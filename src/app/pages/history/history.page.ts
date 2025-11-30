e101a19bdfc9ded285290f2c05ea2407c4e07e68
haizea <96haizea@gmail.com>
2025-11-30 18:49:21 +0100

    History filters

--- src/app/pages/history/history.page.ts
+++ src/app/pages/history/history.page.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { IonDatetime, IonicModule } from '@ionic/angular';
 import { HistoryRepository } from 'src/app/core/services/repositories/history';
 import { AuthService } from 'src/app/core/services/auth';
 import type { User } from '@supabase/supabase-js';
 import type { HistoryItem } from 'src/app/core/models/history.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.page.html',
   styleUrls: ['./history.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule]
+  imports: [CommonModule, IonicModule, FormsModule]
 })
 export class HistoryPage implements OnInit {
-  
+
   user? : User | null;
 
+  selectedDate?: string;
+
   history: HistoryItem[] = [];
+  searchTerm: string = '';
+  filteredItems: HistoryItem[] = [];
+
+  showCalendar : boolean = false;
+
+
 
   constructor(private historyRepository: HistoryRepository, private authService : AuthService) { }
 
@@ -27,12 +36,40 @@ export class HistoryPage implements OnInit {
     // Suscribirse al historial
     this.historyRepository.history$.subscribe(items => {
       this.history = items;
+      this.filteredItems = items;
+
     });
   }
 
-  abrirFiltros() {
-    console.log('Abriendo filtros...');
-    // Funcionalidad futura
+  applyFilters() {
+    this.filteredItems = this.history.filter(item => {
+
+      // Filtrado por texto
+      const textMatch = item.text.toLowerCase().includes(this.searchTerm.toLowerCase());
+
+      // Filtrado por fecha exacta
+      let dateMatch = true;
+      if (this.selectedDate) {
+        const selectedTime = new Date(this.selectedDate).setHours(0,0,0,0);
+        const itemTime = new Date(item.created_at).setHours(0,0,0,0);
+        dateMatch = selectedTime === itemTime;
+      }
+
+      return textMatch && dateMatch;
+    });
+  }
+
+  toggleCalendar() {
+    this.showCalendar = !this.showCalendar; // alterna entre mostrar y ocultar
+  }
+
+  clearDateFilter() {
+    this.selectedDate = '';
+    this.showCalendar = false;
+    this.searchTerm ='';
+    this.applyFilters();
   }
 
 }
+
+
