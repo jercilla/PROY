c4edd356fb297fa6f8c38cb5b5b9d74c76a36922
alexmaestre <contacto@vivazzio.com>
2025-12-07 23:52:19 +0100

    Refactor UserSettings model to use  ToneLabel exportable type for tone property

--- src/app/core/models/user-settings.model.ts
+++ src/app/core/models/user-settings.model.ts
@@ -1,6 +1,7 @@
+export type ToneLabel = 'Muy Informal' | 'Informal' | 'Formal' | 'Muy Formal';
 export interface UserSettings {
   length: number;       // longitud del texto (entre 120 y 1024)
-  tone: 'Muy Informal' | 'Informal' | 'Formal' | 'Muy Formal'; // tono del texto
+  tone: ToneLabel; // tono del texto
   emojis: boolean;      // si se usan emojis
   hashtags: boolean;     // si se usan hashtags
 }
